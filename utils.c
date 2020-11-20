6f75a289b988a649239c8cd74be1d17b1cd0378f
diasgc <15062495+diasgc@users.noreply.github.com>
2020-11-20 15:58:41 +0000

    Update utils.c
    reverse_nbit

--- utils.c
+++ utils.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 unsigned int reverse_nbit(unsigned int input, const unsigned int shift){
+  
+  // no shift no action
+  if (shift == 0)
+    return input;
+
   unsigned int output = 0;
   unsigned int in = input;
+  
+  // set max shift to 7 bits
+  shift = shift & 0xf;
   const unsigned char mask = (1 << shift) - 1;
-  while (in != 0)
-    {
-      output = output << shift;
-      output |= (in & mask);
-      in = in >> shift;
-    }
+  
+  while (in != 0){
+    output = output << shift;
+    output |= (in & mask);
+    in = in >> shift;
+  }
+
   // printf ("mask:%o input:%o output:%o", mask, in, output);
   return output;
 }
