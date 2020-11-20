5a6ee9cb639b0735d3cfb7ae7b13f9051a0805ee
diasgc <15062495+diasgc@users.noreply.github.com>
2020-11-20 19:18:34 +0000

    upd

--- utils.c
+++ utils.c
@@ -5,38 +5,38 @@
 // string.format-like function
 const char *strf(const char *pfmt,...){
 
-  va_list parg;
-  va_start(parg, pfmt);
-  size_t sz;
+    va_list parg;
+    va_start(parg, pfmt);
+    size_t sz;
 
-  sz = snprintf(NULL,0,pfmt,parg);
-  char *pbuff = (char *) malloc(++sz);
+    sz = snprintf(NULL,0,pfmt,parg);
+    char *pbuff = (char *) malloc(++sz);
 
-  vsprintf(pbuff,pfmt,parg);
-  va_end(arg_ptr);
+    vsprintf(pbuff,pfmt,parg);
+    va_end(parg);
 
-  return pbuff;
+    return pbuff;
 }
 
-unsigned int reverse_nbit(unsigned int input, const unsigned int shift){
-  
-  // no shift no action
-  if (shift == 0)
-    return input;
+unsigned int reverse_nbit(unsigned int input, unsigned int shift){
 
-  unsigned int output = 0;
-  unsigned int in = input;
-  
-  // set max shift to 7 bits
-  shift = shift & 0xf;
-  const unsigned char mask = (1 << shift) - 1;
-  
-  while (in != 0){
-    output = output << shift;
-    output |= (in & mask);
-    in = in >> shift;
-  }
+    // no shift no action
+    if (shift == 0)
+        return input;
 
-  // printf ("mask:%o input:%o output:%o", mask, in, output);
-  return output;
+    unsigned int output = 0;
+    unsigned int in = input;
+
+    // set max shift to 7 bits
+    shift = shift & 0xf;
+    const unsigned char mask = (1 << shift) - 1;
+
+    while (in != 0){
+        output = output << shift;
+        output |= (in & mask);
+        in = in >> shift;
+    }
+
+    // printf ("mask:%o input:%o output:%o", mask, in, output);
+    return output;
 }
